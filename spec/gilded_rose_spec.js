99be73e8d9c0832fae2148cfa925f44b1a7c0316
Parsa Jalali <130646144+ItsLeXIX@users.noreply.github.com>
2024-06-19 11:39:13 +0200

    4th spec added: should increase quality of 'Backstage passes' by 2 when there are 10 days or less

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -17,5 +17,11 @@ describe("Gilded Rose", function() {
     update_quality();
     expect(items[0].quality).toEqual(0);
   });
+  
+  it("should increase quality of 'Backstage passes' by 2 when there are 10 days or less", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(22);
+  });
 
 }); 
