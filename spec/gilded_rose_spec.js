762a926dc25c81cd8a943d69c7867260414a66fb
Parsa Jalali <130646144+ItsLeXIX@users.noreply.github.com>
2024-06-19 11:30:57 +0200

    2nd spec added: should decrease quality twice as fast after sell by date (green)

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -6,6 +6,10 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual("foo");
   });
 
-  
+  it("should decrease quality twice as fast after sell by date", function() {
+    items = [ new Item("foo", -1, 5) ];
+    update_quality();
+    expect(items[0].quality).toEqual(3);
+  });
   
 });
